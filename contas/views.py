88a7d9aa948dda8f9a9c1ce64affae239b70744a
r25ta <twentyfive@hotmail.com>
2021-11-06 03:17:16 -0300

    Projeto Django para estudo

--- contas/views.py
+++ contas/views.py
@@ -2,30 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
+# Create your views here.
 def current_datetime(request):
     now = datetime.datetime.now()
-    html="<html><head><h1>Contas</h1></head><body>horário %s</body></html>" %(now)
+    
+    html = "<html><head><h1>Contas</h1></head><body>%s</body></html>" %(now)
     return HttpResponse(html)
-
-def numeros_pares(request):
-    table=""
-    cont_col=1
-    table_row=""
-    
-    for i in range (2000):
-        if (i%2==0):
-            table_row +="<td>%d</td>" %(i)
-            cont_col +=1
-        
-        #QUEBRA POR COLUNAS        
-        if cont_col == 20:
-            cont_col = 1
-            table += "<tr>" + table_row + "</tr>"
-            table_row=""               
-        
-    
-    x = "<table>"+table+"</table>"
-            
-    html= "<html><head><h1>Números Pares</head><body>%s</body></html>" %(x)    
-    return HttpResponse(html)    
-    
